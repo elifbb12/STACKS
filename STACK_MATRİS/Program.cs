da0ac95e5c621d6788b2fb45a06ee5d46a671331
ELİF BÜYÜKBATMAZ <77730991+elifbb12@users.noreply.github.com>
2021-03-21 13:59:40 +0300

    Add files via upload
    "Stack_Matris" dosyası eklendi.

---
+++ STACK_MATRİS/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace STACK_MATRİS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //GÖRÜNTÜ İŞLEME
+            string directoryname1 = @"c:\xampp";
+            Stack<string> st = new Stack<string>();
+            st.Push(directoryname1);
+            DirectoryInfo di = new DirectoryInfo(directoryname1);
+            while (st.Count>0)
+            {
+                directoryname1 = st.Pop();
+                di = new DirectoryInfo(directoryname1);
+                foreach(DirectoryInfo dinf in di.GetDirectories())
+                {
+                    st.Push(dinf.FullName);
+                }
+                foreach(FileInfo fi in di.GetFiles())
+                {
+                    Console.WriteLine(fi.FullName);
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
