307574af8ef9e8b3832bfa2d7f1230730e5dfcaa
ELİF BÜYÜKBATMAZ <77730991+elifbb12@users.noreply.github.com>
2021-03-21 13:58:25 +0300

    Add files via upload
    "Stack Push ve Pop Komutları" eklendi.

---
+++ STACK PUSH VE POP KOMUTLARI/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace STACK_PUSH_VE_POP_KOMUTLARI
+{
+
+    
+
+    class Program
+    {
+
+
+      static int[] stack = new int[100];
+      static int sp = -1; //stackta hiç eleman yok demektir.
+        static void push(int data)
+        {
+            sp++;
+            stack[sp] = data;
+
+        }
+        static int pop()
+        {
+            int data = stack[sp];
+            sp--;
+            return data; //return stack [sp--];
+        }
+     
+
+      static void Main(string[] args)
+      {
+
+            push(10);
+            push(20);
+            Console.WriteLine(pop());
+            Console.WriteLine(pop());
+            Console.ReadLine();
+            //PUSH=İTMEK / STACK'A VERİ EKLENİR.
+            //POP İLE STACKTAN VERİ ALIRIZ.
+            //VERİ YAPISINI OLUŞTURULARIM.
+            //STACKLAR 2 ŞEKİLDE OLUŞUR.(DİZİLER VEYA LİNKED LİSTLERLE)
+            //KULLANICIDAN STRİNG İFADE ALALIM.
+            //{} [] () İÇ İÇE KULLANILABİLİR.
+            
+
+      }
+    }
+}
