508e97e05e3a1643c3830c3e16b784c11c36b09b
ELİF BÜYÜKBATMAZ <77730991+elifbb12@users.noreply.github.com>
2021-03-21 14:05:21 +0300

    Add files via upload
    "Tekli Linked ve Çiftli Linked" dosyası eklendi.

---
+++ TELİ LİNKED-ÇİFTLİ LİNKED/TELİ LİNKED-ÇİFTLİ LİNKED/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TELİ_LİNKED_ÇİFTLİ_LİNKED
+{
+    class Program
+    {
+        class tekli
+        {
+            public int data;
+            public tekli next;  //tekli linked list
+
+        }
+        static void Main(string[] args)
+        {
+            tekli t = new tekli();
+            t.data = 1;
+            tekli w = null;
+            w = t;
+            t.data = 1;
+            // t de adres var t adres tutar = (POİNTER)
+            //TÜM NESNELER ASLINDA BİR POİNTER
+            //4 BYTE
+            //garbage collector temizkler.
+            tekli q = new tekli();
+            q = t;
+            Console.WriteLine(q.data);
+            q.data = 2;
+            Console.ReadLine();
+
+        }
+    }
+}
